646e18bb4ba500166e061a428b4fd2363cc2d4f0
BrandoSoft <csbrandox@gmail.com>
2020-02-23 19:23:29 +0100

    Updates

---
+++ precache-manifest.64958295a72900a9bcfa61a172f400ba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c33ed540cde92d5a29b1b54cfd3ff1",
+    "url": "/simple-react-shop/index.html"
+  },
+  {
+    "revision": "72f6a4ab46cbbb3f7787",
+    "url": "/simple-react-shop/static/css/main.a52fc577.chunk.css"
+  },
+  {
+    "revision": "999271f5f6f237a85589",
+    "url": "/simple-react-shop/static/js/2.41fe9134.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/simple-react-shop/static/js/2.41fe9134.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72f6a4ab46cbbb3f7787",
+    "url": "/simple-react-shop/static/js/main.bf8f0510.chunk.js"
+  },
+  {
+    "revision": "e83488b094b28353d187",
+    "url": "/simple-react-shop/static/js/runtime-main.1c33633e.js"
+  },
+  {
+    "revision": "76dc3d0fadac2432e10a7f3c8aa5530c",
+    "url": "/simple-react-shop/static/media/header1.76dc3d0f.jpg"
+  },
+  {
+    "revision": "9daad6c578f5c99a40bed3821df03eda",
+    "url": "/simple-react-shop/static/media/header2.9daad6c5.jpg"
+  },
+  {
+    "revision": "e431b5cf467f921cc50fab272a7c8fb9",
+    "url": "/simple-react-shop/static/media/header3.e431b5cf.jpg"
+  }
+]);
